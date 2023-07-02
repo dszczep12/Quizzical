ea27e5e14bd34d977e22b08a43e77eacf3bea1c3
Dylan Szczepanski <adorevi9@gmail.com>
2023-07-02 15:09:16 -0500

    basically finished functionality

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -2,22 +2,26 @@ import React, { useEffect, useState } from 'react';
 
 function Quiz(props) {
   const [questions, setQuestions] = useState([]);
+  const [answersChecked, setAnswersChecked] = useState(false);
+  const [quizEnded, setQuizEnded] = useState(false);
 
   function fetchData() {
     fetch('https://opentdb.com/api.php?amount=5&type=multiple')
       .then((response) => response.json())
       .then((data) => {
-        const parsedQuestions = data.results.map((result) => ({
-          question: result.question,
-          answers: result.incorrect_answers.map((answer) => ({
-            text: answer,
-            clicked: false,
-          })),
-          correctAnswer: {
-            text: result.correct_answer,
-            clicked: false,
-          },
-        }));
+        const parsedQuestions = data.results.map((result) => {
+          const answers = [...result.incorrect_answers, result.correct_answer];
+          const shuffledAnswers = shuffleArray(answers);
+          const correctAnswerIndex = shuffledAnswers.findIndex((answer) => answer === result.correct_answer);
+          return {
+            question: result.question,
+            answers: shuffledAnswers.map((answer, index) => ({
+              text: answer,
+              clicked: false,
+            })),
+            correctAnswerIndex: correctAnswerIndex,
+          };
+        });
         console.log(data);
         setQuestions(parsedQuestions);
       })
@@ -30,11 +34,39 @@ function Quiz(props) {
     fetchData();
   }, []);
 
+  const shuffleArray = (arr) => {
+    const shuffledArray = [...arr];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+  };
+
   const btnClicked = (questionIndex, answerIndex) => {
     const updatedQuestions = [...questions];
-    updatedQuestions[questionIndex].answers[answerIndex].clicked = true;
+    updatedQuestions[questionIndex].answers = updatedQuestions[questionIndex].answers.map(
+      (answer, index) => ({
+        ...answer,
+        clicked: index === answerIndex,
+      })
+    );
     setQuestions(updatedQuestions);
-    console.log(questions);
+  };
+
+  const checkAnswer = () => {
+    setAnswersChecked(true);
+
+    // Check if all answers have been checked
+    if (questions.every((question) => question.answers.some((answer) => answer.clicked))) {
+      setQuizEnded(true);
+    }
+  };
+
+  const playAgain = () => {
+    setAnswersChecked(false);
+    setQuizEnded(false);
+    fetchData();
   };
 
   return (
@@ -46,14 +78,25 @@ function Quiz(props) {
             <button
               onClick={() => btnClicked(questionIndex, answerIndex)}
               key={answerIndex}
-              className={`answer ${answer.clicked ? 'clicked' : ''}`}
+              className={`answer ${answer.clicked ? 'clicked' : ''} ${answersChecked && answerIndex === question.correctAnswerIndex ? 'correct' : ''} ${answersChecked && answer.clicked && answerIndex !== question.correctAnswerIndex ? 'incorrect' : ''}`}
             >
               {answer.text}
             </button>
           ))}
         </div>
       ))}
-      <button className="checkAnswer--btn">Check Answer</button>
+      {quizEnded ? (
+        <div>
+          <p>Score: {questions.filter((question) => question.answers[question.correctAnswerIndex].clicked).length}/{questions.length}</p>
+          <button onClick={playAgain} className="playAgain--btn">
+            Play Again
+          </button>
+        </div>
+      ) : (
+        <button onClick={checkAnswer} className="checkAnswer--btn">
+          Check Answer
+        </button>
+      )}
     </div>
   );
 }
